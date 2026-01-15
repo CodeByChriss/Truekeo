48e4ef10c9ebbfa059cc2213fa24f6f539a15993
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2026-01-15 12:07:17 +0100

    quitando dependencias (duplicados) por fallos en el buildeo
    Removed Mapbox dependencies for NDK 27.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("com.mapbox.maps:android-ndk27:11.17.1")
-    implementation("com.mapbox.extension:maps-compose-ndk27:11.17.1")
     implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
