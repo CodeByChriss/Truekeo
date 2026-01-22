b0e2e37084758bec2eb119cd28788911cfbeefe7
ChristianSalguero <salguerovaras777@gmail.com>
2026-01-22 13:18:52 +0100

    import no usado eliminado

--- app/src/main/java/com/chaima/truekeo/components/MessageCard.kt
+++ app/src/main/java/com/chaima/truekeo/components/MessageCard.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
