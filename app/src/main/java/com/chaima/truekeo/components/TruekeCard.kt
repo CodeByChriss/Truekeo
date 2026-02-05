fab15a7fb07531b47392074f5274f738cef3d97c
malmorox <marcos.almorox@gmail.com>
2026-02-05 12:55:05 +0100

    Ajustes en imagenes y limpieza de código en varios archivos

--- app/src/main/java/com/chaima/truekeo/components/TruekeCard.kt
+++ app/src/main/java/com/chaima/truekeo/components/TruekeCard.kt
@@ -1,7 +1,6 @@
 package com.chaima.truekeo.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -33,10 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
@@ -50,7 +46,6 @@ import com.chaima.truekeo.R
 import com.chaima.truekeo.models.Item
 import com.chaima.truekeo.models.Trueke
 import com.chaima.truekeo.models.TruekeStatus
-import com.chaima.truekeo.models.User
 import com.chaima.truekeo.utils.TimePrefix
 import com.chaima.truekeo.utils.prefixedTimeAgo
 
